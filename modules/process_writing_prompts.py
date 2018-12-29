b4b35a701864e692fa9bc93f4886fb41f27ad4c1
henrye <c.henry.elder@gmail.com>
2018-12-29 12:00:22 +0000

    spacy does its own multiprocessing

--- modules/process_writing_prompts.py
+++ modules/process_writing_prompts.py
@@ -1,66 +1,41 @@
 import argparse
 import os
 import re
-import concurrent.futures
+# import concurrent.futures
 import spacy
+from multiprocessing import Pool, cpu_count
 
 from tqdm import tqdm
 
-# regexes
-match_newline_tokens = re.compile(r"(<newline>)(\s\1){0,}")
-match_ellipses = re.compile(r"\s\.\.\.\s")
-# To make sure we don't loose the surrounding letters sub using r"\1'\3"
-match_single_close_quote = re.compile(r"(\w)(\s’\s)(\w{1,2}\b)")
-match_double_open_quote = re.compile(r"``")
-match_double_close_quote = re.compile(r"''")
-# Again join at the end using \1\2, these special contractions get double
-# tokenized by spacy. ugh
-match_special_contractions = re.compile(r"(\b\w+)\s('(ve|m))\b")
-# this is for splitting apart quotations and newline characters
-match_split_chars = re.compile(r"(“|”|newwline)")
-
-# This probably shouldn't be a global variable but I'm used to putting stuff in
-# the main() function but it's difficult to pass stuff around with the new
-# execute pooled function
-nlp = spacy.load('en_core_web_sm')
 
 # TODO
 # Add back in the writing prompt input option once we get multithreading
 # working ok
-def sent_tok_story(story):
-    story = story.strip()
-    # A whole bunch of regex substitutions
-    story = re.sub(match_newline_tokens, r"newwline", story)
-    # run this twice in case there are two in a row
-    story = re.sub(match_ellipses, r" … ", story)
-    story = re.sub(match_ellipses, r" … ", story)
-    story = re.sub(match_single_close_quote, r"\1'\3", story)
-    story = re.sub(match_double_open_quote, r"“", story)
-    story = re.sub(match_double_close_quote, r"”", story)
-    story = re.sub(match_special_contractions, r"\1\2", story)
-    # TODO
-    # Maybe we can speed things up by setting a max number of
-    # tokens before running the spacy tokenizer. Maybe split on
-    # spaces and limit to 1,100 tokens, then rejoin.
-    story_doc = nlp(story)
-    sents = []
-    num_tokens = 0
-    for sent in story_doc.sents:
-        tokens = [token.text for token in sent]
-        num_tokens += len(tokens)
-        # like in the original we limit to 1,000 tokens
-        if num_tokens >= 1000:
-            break
-        sents.append(' '.join(tokens))
-    # Here we break apart quotation marks and newlines into their
-    # own separate lines
-    final_lines = []
-    for sent in sents:
-        split_sent = re.split(match_split_chars, sent)
-        split_sent = [this.strip() for this in split_sent]
-        split_sent = list(filter(None, split_sent))
-        final_lines.extend(split_sent)
-    return final_lines
+# def sent_tok_story(story):
+#     story = story.strip()
+#     # TODO
+#     # Maybe we can speed things up by setting a max number of
+#     # tokens before running the spacy tokenizer. Maybe split on
+#     # spaces and limit to 1,100 tokens, then rejoin.
+#     story_doc = nlp(story)
+#     sents = []
+#     num_tokens = 0
+#     for sent in story_doc.sents:
+#         tokens = [token.text for token in sent]
+#         num_tokens += len(tokens)
+#         # like in the original we limit to 1,000 tokens
+#         if num_tokens >= 1000:
+#             break
+#         sents.append(' '.join(tokens))
+#     # Here we break apart quotation marks and newlines into their
+#     # own separate lines
+#     final_lines = []
+#     for sent in sents:
+#         split_sent = re.split(match_split_chars, sent)
+#         split_sent = [this.strip() for this in split_sent]
+#         split_sent = list(filter(None, split_sent))
+#         final_lines.extend(split_sent)
+#     return final_lines
 
 def main():
     parser = argparse.ArgumentParser(description='Process and tokenize into'
@@ -75,6 +50,26 @@ def main():
     args = parser.parse_args()
 
 
+    # regexes
+    match_newline_tokens = re.compile(r"(<newline>)(\s\1){0,}")
+    match_ellipses = re.compile(r"\s\.\.\.\s")
+    # To make sure we don't loose the surrounding letters sub using r"\1'\3"
+    match_single_close_quote = re.compile(r"(\w)(\s’\s)(\w{1,2}\b)")
+    match_double_open_quote = re.compile(r"``")
+    match_double_close_quote = re.compile(r"''")
+    # Again join at the end using \1\2, these special contractions get double
+    # tokenized by spacy. ugh
+    match_special_contractions = re.compile(r"(\b\w+)\s('(ve|m))\b")
+    # this is for splitting apart quotations, newline characters and special
+    # start and end writing prompt ones
+    match_split_chars = re.compile(r"(“|”|newwline|staart\w*|ennd\w*)")
+    # writing prompts at the start
+    match_wp_acronym = re.compile(r"^\[ (\w*) \] (.*)")
+
+    # This probably shouldn't be a global variable but I'm used to putting stuff in
+    # the main() function but it's difficult to pass stuff around with the new
+    # execute pooled function
+    nlp = spacy.load('en_core_web_sm')
 
     # for input_file_name in args.input_file_names:
     #     file_extension = os.path.splitext(input_file_name)[1]
@@ -93,7 +88,53 @@ def main():
     with open(args.wp_file_name) as wp_file, open(args.stories_file_name) as \
         stories_file, open(output_file_name, 'w') as out_file:
 
-        stories = stories_file.readlines()
+        stories_preprocessed = []
+        num_stories = 0
+        for writing_prompt, story in zip(wp_file, stories_file):
+            wp_processed = re.sub(match_wp_acronym, r"staart\1 \2 ennd\1",
+                                  writing_prompt.strip())
+            wp_and_story = wp_processed + ' ' + story.strip()
+            # We're going to try and speed up things a little by dropping out
+            # tokens we don't need. Since we're only taking up to 1000 tokens
+            wp_and_story = ' '.join(wp_and_story.split()[:1100])
+            # A whole bunch of regex substitutions
+            wp_and_story = re.sub(match_newline_tokens, r"newwline", wp_and_story)
+            # run this twice in case there are two in a row
+            wp_and_story = re.sub(match_ellipses, r" … ", wp_and_story)
+            wp_and_story = re.sub(match_ellipses, r" … ", wp_and_story)
+            wp_and_story = re.sub(match_single_close_quote, r"\1'\3", wp_and_story)
+            wp_and_story = re.sub(match_double_open_quote, r"“", wp_and_story)
+            wp_and_story = re.sub(match_double_close_quote, r"”", wp_and_story)
+            wp_and_story = re.sub(match_special_contractions, r"\1\2", wp_and_story)
+            stories_preprocessed.append(wp_and_story)
+            num_stories += 1
+            if num_stories >= args.max_num_stories:
+                break
+
+        sentence_tokenized_stories = []
+        pbar = tqdm(total=args.max_num_stories)
+        for story_doc in nlp.pipe(stories_preprocessed):
+            pbar.update(1)
+            sents = []
+            num_tokens = 0
+            for sent in story_doc.sents:
+                tokens = [token.text for token in sent]
+                num_tokens += len(tokens)
+                # like in the original we limit to a max of 1,000 tokens
+                if num_tokens >= 1000:
+                    break
+                sents.append(' '.join(tokens))
+            # Here we break apart quotation marks and newlines into their
+            # own separate lines
+            final_lines = []
+            for sent in sents:
+                split_sent = re.split(match_split_chars, sent)
+                split_sent = [this.strip() for this in split_sent]
+                split_sent = list(filter(None, split_sent))
+                final_lines.extend(split_sent)
+            sentence_tokenized_stories.append('\n'.join(final_lines))
+        out_file.write('\n\n'.join(sentence_tokenized_stories) + "\n\n")
+
 
         # with open(input_file_name) as in_file:
         #     stories = in_file.readlines()
@@ -106,19 +147,23 @@ def main():
         # with open(output_file_name, 'w') as out_file:
         #     for story in tqdm(stories):
                 # is it bad that we edit the same variable?
-        sentence_tokenized_stories = []
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            num_tokenized_stories = 0
-            pbar = tqdm(total=args.max_num_stories)
-            for sentence_tokenized_story in executor.map(sent_tok_story,
-                                                         stories,
-                                                         chunksize=1001):
-                sentence_tokenized_stories.append('\n'.join(sentence_tokenized_story))
-                num_tokenized_stories += 1
-                pbar.update(1)
-                if num_tokenized_stories >= args.max_num_stories:
-                    break
-        out_file.write('\n\n'.join(sentence_tokenized_stories) + "\n\n")
+        # sentence_tokenized_stories = []
+        # num_cpus = cpu_count()
+        # with Pool(processes=num_cpus) as executor:
+        # # with concurrent.futures.ProcessPoolExecutor() as executor:
+        #     # num_tokenized_stories = 0
+        #     this = executor.map(sent_tok_story, stories)
+        # this = [sent_tok_story(story) for story in stories]
+            # import ipdb; ipdb.set_trace()
+            # pbar = tqdm(total=args.max_num_stories)
+            # for sentence_tokenized_story in executor.map(sent_tok_story,
+            #                                              stories):
+            # sentence_tokenized_stories.append('\n'.join(sentence_tokenized_story))
+            # num_tokenized_stories += 1
+            # pbar.update(1)
+            # if num_tokenized_stories >= args.max_num_stories:
+            #     break
+        # out_file.write('\n\n'.join(sentence_tokenized_stories) + "\n\n")
 
 if __name__ == '__main__':
     main()
