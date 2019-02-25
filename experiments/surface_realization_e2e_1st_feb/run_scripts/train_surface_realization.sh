7f20c0d519b272d50961b943180facc40776dde3
Henry-E <henry.elder@adaptcentre.ie>
2019-02-25 09:36:39 +0000

    new surface realization experiments

--- experiments/surface_realization_e2e_1st_feb/run_scripts/train_surface_realization.sh
+++ experiments/surface_realization_e2e_1st_feb/run_scripts/train_surface_realization.sh
@@ -3,9 +3,9 @@
 # 14m_sents
 
 python /home/henrye/downloads/Henry_OpenNMT-py/train.py \
-	-data preprocess/1st_feb_tests/e2e_surface_realization \
-	-save_model train/1st_feb_tests/e2e_surface_realization \
-	--log_file train/1st_feb_tests/log.txt \
+	-data preprocess/22nd_feb_tests/e2e_surface_realization \
+	-save_model train/25th_feb_tests/e2e_surface_realization \
+	--log_file train/25th_feb_tests/log.txt \
 	--optim adam \
 	--learning_rate 0.001 \
 	--encoder_type brnn \
@@ -13,12 +13,14 @@ python /home/henrye/downloads/Henry_OpenNMT-py/train.py \
 	--batch_size 64 \
 	--word_vec_size 300 \
 	--rnn_size 450 \
+	--share_embeddings \
 	--copy_attn \
 	--reuse_copy_attn \
 	--report_every 500 \
-	--valid_steps 1000 \
-	--save_checkpoint 3000 \
+	--valid_steps 500 \
+	--save_checkpoint 500 \
 	--train_steps 12000 \
-	--start_decay_steps 720000 \
-	--gpu_ranks 0 \
-	--world_size 1
+	--start_decay_steps 6000 \
+	--decay_steps 2000 \
+	--gpu_ranks 0 1 \
+	--world_size 2
