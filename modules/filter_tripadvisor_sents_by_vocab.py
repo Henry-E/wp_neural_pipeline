dbc3aa9ed27dcbb9d1fade52c2d703b16a175139
henrye <c.henry.elder@gmail.com>
2019-03-07 10:56:13 +0000

    setting up for snlp parsed files

--- modules/filter_tripadvisor_sents_by_vocab.py
+++ modules/filter_tripadvisor_sents_by_vocab.py
@@ -9,14 +9,14 @@ import numpy
 def accumulate_percentages(percent_in_vocab, min_percent):
     num_qualifying_sents = 0
     for percent, num_sents in percent_in_vocab.items():
-        if percent > min_percent:
+        if min_percent <= percent:
             num_qualifying_sents += num_sents
     return  num_qualifying_sents
 
 def main():
     parser = argparse.ArgumentParser(description='tag, filter and create '
                                      'source')
-    # parser.add_argument('-o', '--output_dir_name', help='output directory')
+    parser.add_argument('-o', '--output_dir_name', help='output directory')
     parser.add_argument('-i', '--input_file_name', help='tripadvisor conllu'
                         ' file')
     parser.add_argument('-v', '--vocab_file_name', help='vocab from e2e')
@@ -28,6 +28,7 @@ def main():
     trip_advisor_sents = pyconll.load.iter_from_file(args.input_file_name)
 
     total_num_sents = 0
+    all_sents_percent_in_vocab = []
     percent_in_vocab = Counter()
     for sent in trip_advisor_sents:
         total_num_tokens = 0
@@ -36,7 +37,8 @@ def main():
             this_form = token.form
             if not this_form:
                 pass
-            if this_form in vocab:
+            # making sure to lower case form since vocab is all lower case
+            if this_form.lower() in vocab:
                 num_tokens_in_vocab += 1
             total_num_tokens += 1
         try:
@@ -45,15 +47,33 @@ def main():
         except:
             this_percent_in_vocab = 0
         percent_in_vocab[this_percent_in_vocab] += 1
+        all_sents_percent_in_vocab.append(this_percent_in_vocab)
         total_num_sents += 1
 
     print(total_num_sents)
-    print('min %', 'total', 'as a %')
-    for this_percent in numpy.arange(1, 0.5, -0.05):
-        this_many_sents = accumulate_percentages(percent_in_vocab, this_percent)
-        print(round(this_percent, 2), this_many_sents, 
-                round(this_many_sents / total_num_sents, 2))
+    print('\t'.join(['min %', 'total', 'as a %']))
+    for min_percent in numpy.arange(1, 0.5, -0.05):
+        this_many_sents = accumulate_percentages(percent_in_vocab, min_percent)
+        print('\t'.join(str(x) for x in [round(min_percent, 2),
+                                         this_many_sents,
+                                         round(this_many_sents / total_num_sents, 2)]))
 
+    # let's see how slow this is first before trying to optimize
+    for min_percent in numpy.arange(1, 0.5, -0.05):
+        trip_advisor_sents = pyconll.load.iter_from_file(args.input_file_name)
+        out_conllu = []
+        for this_percent, sent in tqdm(zip(all_sents_percent_in_vocab,
+                                           trip_advisor_sents),
+                                       total=total_num_sents):
+            if min_percent <= this_percent:
+                out_conllu.append(sent.conll())
+        input_file_root = \
+            os.path.basename(os.path.splitext(args.input_file_name)[0])
+        output_file_name = os.path.join(args.output_dir_name,
+                                        input_file_root +
+                        '_min_{}_percent_overlap.conllu'.format(min_percent))
+        with open(output_file_name, 'w') as out_file:
+            out_file.write('\n'.join(out_conllu))
 
 if __name__ == '__main__':
     main()
--- modules/get_vocab_from_conllu.py
+++ modules/get_vocab_from_conllu.py
@@ -9,32 +9,42 @@ def main():
     parser = argparse.ArgumentParser(description='''run through all the words in
                                      a conllu file and get a vocabulary based
                                      on min numbers of occurences''')
-    parser.add_argument('-i', '--input_file_name', help='a train conllu file')
+    parser.add_argument('-i', '--input_file_names', nargs='*',
+                        help='trainset conllu files')
     parser.add_argument('-o', '--output_dir_name', help='output dir for vocab')
     parser.add_argument('--min_num_occurrences', default=10, type=int,
-                        help='''the minimum number of times a word must appear
-                                in the corpus to be part of the vocab file''')
+                        help='the minimum number of times a word must appear'
+                        ' in the corpus to be part of the vocab file')
+    parser.add_argument('--max_vocab_size', default=50000, type=int,
+                        help='max vocab allowed, this is the usual size,'
+                        'however it will be a little larger with unk tokens')
     args = parser.parse_args()
 
-    conllu_sents = pyconll.load.iter_from_file(args.input_file_name)
-    all_the_words = Counter()
-    for sent in tqdm(conllu_sents):
-        for token in sent:
-            if token.form:
-                all_the_words[token.form] += 1
-    vocab_out = []
-    # only take words which appear min_num_occurences times
-    for word, num_occurences in tqdm(all_the_words.most_common()):
-        if num_occurences < args.min_num_occurrences:
-            break
-        vocab_out.append(word)
+    for input_file_name in args.input_file_names:
+        conllu_sents = pyconll.load.iter_from_file(input_file_name)
+        all_the_words = Counter()
+        for sent in tqdm(conllu_sents):
+            for token in sent:
+                if token.form:
+                    # we're lower casing all vocabs for philosophical reasons;
+                    # the model already gets enough information from the int rep
+                    all_the_words[token.form.lower()] += 1
+        vocab_out = []
+        # only take words which appear min_num_occurences times
+        for word, num_occurences in tqdm(all_the_words.most_common()):
+            if num_occurences < args.min_num_occurrences:
+                break
+            vocab_out.append(word)
 
-    input_file_root = \
-        os.path.basename(os.path.splitext(args.input_file_name)[0])
-    output_file_name = os.path.join(args.output_dir_name,
-                                    input_file_root + '.vocab.txt')
-    with open(output_file_name, 'w') as out_file:
-        out_file.write('\n'.join(vocab_out))
+        # max vocab size
+        vocab_out = vocab_out[:args.max_vocab_size]
+
+        input_file_root = \
+            os.path.basename(os.path.splitext(input_file_name)[0])
+        output_file_name = os.path.join(args.output_dir_name,
+                                        input_file_root + '.vocab.txt')
+        with open(output_file_name, 'w') as out_file:
+            out_file.write('\n'.join(vocab_out))
 
 if __name__ == '__main__':
     main()
